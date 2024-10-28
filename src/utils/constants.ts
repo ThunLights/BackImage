b557f81aabbb755fa2b9ef4fd9a38ab4371c7ef0
ROBOT <tkata12@outlook.jp>
2024-10-28 22:51:24 +0900

    add backup file path

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,10 @@
 import pkg from '../../package.json';
+import { cssPath, jsPath } from './vscodePath';
 
 export const VERSION = pkg.version;
 
 export const ENCODING = "utf-8";
+
+export const BACKUP_JS_PATH = jsPath + ".backup";
+
+export const BACKUP_CSS_PATH = cssPath + ".backup";
