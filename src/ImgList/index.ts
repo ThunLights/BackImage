2c4eddcd0522f15938ab805b18aaf86b7683631d
ROBOT <tkata12@outlook.jp>
2024-11-02 23:48:59 +0900

    backgroundTypeに関する項目を追加

--- src/ImgList/index.ts
+++ src/ImgList/index.ts
@@ -1,34 +1,47 @@
-import fs from "fs";
-import path from "path";
-
 import { ExtensionContext } from "vscode";
+
+type fullscreenOther = "panel" | "side-bar" | "editor";
+
+export type BackgroundType = "fullscreen" | fullscreenOther[] | null;
 
 export type Folder = {
     id: string
     name: string
     description: string
     path: string
-}
+};
 
 export class FolderController {
     public static readonly _globalStateKey = "imageLists";
+    public static readonly _backgroundStateKey = "backgroundType";
 
     constructor(private _context: ExtensionContext) {
+    }
+
+    public get backgroundType() {
+        return this._context.globalState.get<BackgroundType>(FolderController._backgroundStateKey) ?? null;
     }
 
     public get imageLists() {
         return this._context.globalState.get<Folder[]>(FolderController._globalStateKey) ?? [];
     }
+
     public addImgList(folder: Folder) {
         const imgList = this.imageLists;
         imgList.push(folder);
         this._context.globalState.update(FolderController._globalStateKey, imgList);
         return imgList;
     }
+
     public removeImgList(folder: Folder) {
         let imgList = this.imageLists;
         imgList = imgList.filter(value => value !== folder);
         this._context.globalState.update(FolderController._globalStateKey, imgList);
         return imgList;
+    }
+
+    public updateBackgroundType(content: BackgroundType) {
+        this._context.globalState.update(FolderController._backgroundStateKey, content);
+        return this.backgroundType;
     }
 }
